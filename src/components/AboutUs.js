2364a1c5b33296e4a5a0bee829a7585a0030589c
bahizi2 <bahizimwami2@gmail.com>
2023-04-12 15:36:01 +0300

    Delet a paragraph in aboutjs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,13 +3,6 @@ import "./AboutUsStyles.css";
 function AboutUs() {
   return (
     <div className="about-container">
-      <h1>Your Code Here</h1>
-      <p>
-        Ever wonder what “image” might look like in the mix? Well, you're in
-        luck! We've cooked up some examples of how you could use the “image”
-        icon in your projects, whether they’re apps, interfaces, or print
-        designs.
-      </p>
 
       <h1>Your Code Here</h1>
       <p>
