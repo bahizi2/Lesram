154802021540162fbcfdfcfda697c7ba788169a3
bahizi2 <bahizimwami2@gmail.com>
2023-04-18 20:46:04 +0300

    addind alice pic

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import Destination from "../components/Destination";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import Trip from "../components/Trip";
+import TripHome from "../components/TripHome";
 
 function Home() {
   return (
