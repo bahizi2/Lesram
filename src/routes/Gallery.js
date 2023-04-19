65e0d60590e811597eda9671c5ac03adc3678f49
bahizi2 <bahizimwami2@gmail.com>
2023-04-19 16:53:12 +0300

    gallery section added

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -2,7 +2,8 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import AboutImg from "../assets/2.jpg";
 import Footer from "../components/Footer";
-
+import SimpleReactLightbox from "simple-react-lightbox";
+import Gallery from "../components/Gallery"
 function Gallery() {
   return (
     <>
@@ -16,6 +17,9 @@ function Gallery() {
         // url="/"
         // btnClass="show"
       />
+      <SimpleReactLightbox>
+      <Gallery/>
+      </SimpleReactLightbox>
       <Footer />
     </>
   );
