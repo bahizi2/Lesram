de00a6486c069d0328b972f03537d3b97d4b29c3
bahizi m <bahizimwami2@gmail.com>
2023-04-24 13:05:49 +0000

    Update ContactForm.js and Gallery.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,8 +9,15 @@ function ContactForm() {
         <input placeholder="Email" />
         <input placeholder="Subject" />
         <textarea placeholder="Message" row="4"></textarea>
-        <button>Send Message</button>
+        <button className="show">Send Message</button>
       </form>
+      {/* <form>
+                <input type="text" name="First Name" placeholder="First Name" required>
+                <input type="text" name="Last Name" placeholder="Last Name" required>
+            <input type="email" name="Email Address" placeholder="Your Email Address" required>
+            <textarea name="Message"   rows="7" placeholder="Message" required></textarea>
+            <button type="submit" class="btn btn-primary">Send Message</button>
+        </form> */}
     </div>
   );
 }
--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import AboutImg from "../assets/2.jpg";
 import Footer from "../components/Footer";
-import Gallery from "../components/Gallery"
+// import Galleryp from "../components/Galleryp";
 function Gallery() {
   return (
     <>
